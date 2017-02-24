00d272a16ec54a8d7d55b8070be705a544a3d8b8
Adam Jahur <adamnjahur@gmail.com>
2017-02-24 01:32:23 -0500

    finished orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,52 @@ var connection = require('./connection');
 //notify server administrator that the orm is connected
 console.log("orm connected");
 
+var orm = {
+	getUserAverageCostsByMealType : function(req, callback) {
 
+		connection.query('SELECT cost FROM food WHERE mealtype = ? AND username = ?', [req.body.username] function(err, costs) {
+
+			if(err) throw err;
+
+			var sum = 0;
+
+			for (i = 0; i < costs.length; i++){
+
+				sum = sum + costs[i];
+
+			}
+
+			var IndividualUserAvgCost = sum/costs.length;
+
+			callback(IndividualUserAvgCost);
+
+		});
+
+	},
+
+	getAllUsersAverageCostsByMealType : function(req, callback) {
+
+		connection.query('SELECT cost FROM food WHERE mealtype = ?', [req.body.mealtype] function(err, costs) {
+
+			if(err) throw err;
+
+			var sum = 0;
+
+			for (i = 0; i < costs.length; i++) {
+
+				sum = sum + costs[i];
+			}
+
+			var allUserAvgCost = sum/costs.length;
+
+			callback(allUserAvgCost);
+
+		});
+
+	},
+
+};
 
 
 module.exports = orm;
+
